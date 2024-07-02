a8ea95b271d5357f135026cfa28949e465ca1a1e
rafaelrsry <rafaelrsry@gmail.com>
2024-07-02 14:09:48 -0500

    Primera pregunta primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_RIOS/DswiiT3RiosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_RIOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3RiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3RiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_RIOS/DswiiT3RiosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_RIOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3RiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
