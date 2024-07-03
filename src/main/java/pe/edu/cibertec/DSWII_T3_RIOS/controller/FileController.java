5ead82434d1817726bc042012830309631490394
rafaelrsry <rafaelrsry@gmail.com>
2024-07-02 22:14:00 -0500

    tercera pregunta primer commit

--- src/main/java/pe/edu/cibertec/DSWII_T3_RIOS/controller/FileController.java
+++ src/main/java/pe/edu/cibertec/DSWII_T3_RIOS/controller/FileController.java
@@ -1,4 +1,35 @@
 package pe.edu.cibertec.DSWII_T3_RIOS.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import pe.edu.cibertec.DSWII_T3_RIOS.model.dto.ArchivoDto;
+import pe.edu.cibertec.DSWII_T3_RIOS.service.FileService;
+
+import java.util.List;
+
+@PreAuthorize("hasRole('ADMIN')")
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/files")
 public class FileController {
+
+    private FileService fileService;
+
+    @PostMapping("")
+    public ResponseEntity<ArchivoDto> subirArchivos(
+            @RequestParam("files") List<MultipartFile> multipartFileList
+    ) throws Exception{
+        fileService.guardarArchivos(multipartFileList);
+        return new ResponseEntity<>(ArchivoDto.builder()
+                .mensaje("Archivos subidos correctamente").build(),
+                HttpStatus.OK);
+    }
+
 }
--- src/main/java/pe/edu/cibertec/DSWII_T3_RIOS/service/IFileService.java
+++ src/main/java/pe/edu/cibertec/DSWII_T3_RIOS/service/IFileService.java
@@ -1,4 +1,12 @@
 package pe.edu.cibertec.DSWII_T3_RIOS.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface IFileService {
+
+    void guardarArchivo(MultipartFile archivo) throws Exception;
+
+    void guardarArchivos(List<MultipartFile> archivosList) throws Exception;
 }
